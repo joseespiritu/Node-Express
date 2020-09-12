c6b8bdb0951c046530a321bc3b95bc3b3768417c
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-09-12 14:17:32 -0500

    Simple server request

--- basics/script3.js
+++ basics/script3.js
@@ -1,8 +1,14 @@
-var server = require('http'); //http is a module
+var http = require('http'); //http is a module
+var server = http.createServer(engine);
 
-server.createServer(engine).listen(1337); // leefs
+server.listen(1337, function() {
+    console.log('Server was hit by a request');
+});
+
+/* server.createServer(engine).listen(1337); // leefs */
 
 function engine(request, response) {
+    //console.log(response);
     response.writeHead(200, { 'Content-Type': 'text/plain', 'blablabla': 'from code.com' });
-    response.end('Hey there, from the server');
+    response.end('Hey there, from the server :D. Request from page: ' + request.url);
 }
