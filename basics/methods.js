cfe6da00fe6602ca09363901e8a69c633ba835b2
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-09-12 13:48:32 -0500

    Require Function

---
+++ basics/methods.js
@@ -0,0 +1,21 @@
+var methods = {};
+
+this.output = 1337;
+
+methods.sumNumbers = function(a, b) {
+    output = a + b;
+    return a + b;
+}
+
+methods.circleArea = function(radius) {
+    output = 2 * Math.PI * radius;
+    return 2 * Math.PI * radius;
+}
+
+methods.areaOfRectangle = function(a, b) {
+    output = a + b;
+    return a + b;
+}
+
+exports.data = methods;
+/* exports.output = output; */
